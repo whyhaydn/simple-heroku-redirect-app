0f5e6e536387d1e933555768c169171943bee934
Haydn <65425517+whyhaydn@users.noreply.github.com>
2022-08-01 12:22:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
     <meta http-equiv="refresh" content="0;URL='<?php echo getenv('https://solo.to/holyunblocker') ?>'" />
   </head>
   <body>
-    <p>Please visit the official <?php echo getenv('Holy Unblocker, From Rift') ?> site at <a href="<?php echo getenv('https://solo.to/holyunblocker') ?>"><?php echo getenv('https://solo.to/holyunblocker') ?></a>.</p>
+    <p>Please visit the official <?php echo getenv('Holy Unblocker, From Rift') ?> site at <a href="https://solo.to/holyunblocker/"> Holy Unblocker, From Rift</a><a href="<?php echo getenv('https://solo.to/holyunblocker') ?>"><?php echo getenv('https://solo.to/holyunblocker') ?></a>.</p>
   </body>
 </html>
